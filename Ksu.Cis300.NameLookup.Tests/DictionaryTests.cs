0d71cbc5c1f5a37bde017d1b70081ca3ac8a11d9
varenhorst <alexvarenhorst@gmail.com>
2019-02-25 15:04:46 -0600

    Lab 14 Commits!

--- Ksu.Cis300.NameLookup.Tests/DictionaryTests.cs
+++ Ksu.Cis300.NameLookup.Tests/DictionaryTests.cs
@@ -1,1 +1,200 @@
 ﻿// Unit tests will go here.
+
+/* DictionaryTests.cs
+ * Author: Rod Howell
+ */
+using NUnit.Framework;
+using System;
+
+namespace Ksu.Cis300.NameLookup.Tests
+{
+    /// <summary>
+    /// Unit tests for the Dictionary class.
+    /// </summary>
+    [TestFixture]
+    public class DictionaryTests
+    {
+        /// <summary>
+        /// Gets a dictionary containing five keys and values.
+        /// </summary>
+        private Dictionary<int, string> LoadDictionary()
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            d.Add(10, "Ten");
+            d.Add(5, "Five");
+            d.Add(15, "Fifteen");
+            d.Add(7, "Seven");
+            d.Add(20, "Twenty");
+            return d;
+        }
+        /// <summary>
+        /// Tests that a correct exception is thrown when a null key is added.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestAAddNullKey()
+        {
+            Dictionary<string, float> d = new Dictionary<string, float>();
+            Exception ex = null;
+            try
+            {
+                d.Add(null, 0);
+            }
+            catch (Exception e)
+            {
+                ex = e;
+            }
+            Assert.That(ex, Is.Not.Null.And.TypeOf(typeof(ArgumentNullException)));
+        }
+
+        /// <summary>
+        /// Tests that a correct exception is thrown when a null key is looked up.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestALookUpNullKey()
+        {
+            Dictionary<string, double> d = new Dictionary<string, double>();
+            Exception ex = null;
+            double x;
+            try
+            {
+                d.TryGetValue(null, out x);
+            }
+            catch (Exception e)
+            {
+                ex = e;
+            }
+            Assert.That(ex, Is.Not.Null.And.TypeOf(typeof(ArgumentNullException)));
+        }
+
+        /// <summary>
+        /// Tests a lookup on an empty dictionary. TryGetValue should return false
+        /// and set its out parameter to null.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestBLookUpEmpty()
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            string s;
+            Assert.Multiple(() =>
+            {
+                Assert.That(d.TryGetValue(7, out s), Is.False);
+                Assert.That(s, Is.Null);
+            });
+        }
+
+        /// <summary>
+        /// Adds one key and value, then looks it up.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestCAddOneLookItUp()
+        {
+            Dictionary<string, int> d = new Dictionary<string, int>();
+            d.Add("one", 1);
+            int i;
+            Assert.Multiple(() =>
+            {
+                Assert.That(d.TryGetValue("one", out i), Is.True);
+                Assert.That(i, Is.EqualTo(1));
+            });
+        }
+
+        /// <summary>
+        /// Tests that the proper exception is thrown if a duplicate key is added.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestDAddDuplicate()
+        {
+            Dictionary<string, int> d = new Dictionary<string, int>();
+            d.Add("two", 2);
+            d.Add("one", 1);
+            Exception ex = null;
+            try
+            {
+                d.Add("two", 2);
+            }
+            catch (Exception e)
+            {
+                ex = e;
+            }
+            Assert.That(ex, Is.Not.Null.And.TypeOf(typeof(ArgumentException)));
+        }
+
+        /// <summary>
+        /// Adds five keys and values, then looks up the one that should be
+        /// first in the list.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestDLookUpFirst()
+        {
+            Dictionary<int, string> d = LoadDictionary();
+            string s;
+            Assert.Multiple(() =>
+            {
+                Assert.That(d.TryGetValue(5, out s), Is.True);
+                Assert.That(s, Is.EqualTo("Five"));
+            });
+        }
+
+        /// <summary>
+        /// Adds five keys and values, then looks up the one that should be last
+        /// in the list.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestDLookUpLast()
+        {
+            Dictionary<int, string> d = LoadDictionary();
+            string s;
+            Assert.Multiple(() =>
+            {
+                Assert.That(d.TryGetValue(20, out s), Is.True);
+                Assert.That(s, Is.EqualTo("Twenty"));
+            });
+        }
+
+        /// <summary>
+        /// Adds five keys and values, then looks up a key smaller than any in the dictionary.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestDLookUpSmaller()
+        {
+            Dictionary<int, string> d = LoadDictionary();
+            string s;
+            Assert.Multiple(() =>
+            {
+                Assert.That(d.TryGetValue(3, out s), Is.False);
+                Assert.That(s, Is.Null);
+            });
+        }
+
+        /// <summary>
+        /// Adds five keys and values, then looks up a nonexistent key belonging in the
+        /// middle of the list.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestDLookUpMiddle()
+        {
+            Dictionary<int, string> d = LoadDictionary();
+            string s;
+            Assert.Multiple(() =>
+            {
+                Assert.That(d.TryGetValue(13, out s), Is.False);
+                Assert.That(s, Is.Null);
+            });
+        }
+
+        /// <summary>
+        /// Adds five keys and values, then looks up a key larger than any in the dictionary.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestDLookUpGreater()
+        {
+            Dictionary<int, string> d = LoadDictionary();
+            string s;
+            Assert.Multiple(() =>
+            {
+                Assert.That(d.TryGetValue(25, out s), Is.False);
+                Assert.That(s, Is.Null);
+            });
+        }
+    }
+}
---
+++ Ksu.Cis300.NameLookup/Dictionary.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.NameLookup.Tests
+{
+    public class Dictionary<TKey, TValue> where TKey : IComparable<TKey>
+    {
+        private LinkedListCell<KeyValuePair<TKey, TValue>> _header = new LinkedListCell<KeyValuePair<TKey, TValue>>();
+
+        private static void CheckNull(TKey x)
+        {
+            if (x == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        //Method to get the last cell. It returns the last cell, less than the x value
+
+        private LinkedListCell<KeyValuePair<TKey, TValue>> GetLastCell(TKey x)
+        {
+            LinkedListCell<KeyValuePair<TKey, TValue>> temp = _header;
+            while (temp.Next != null && temp.Next.Data.Key.CompareTo(x) < 0)
+            {
+                temp = temp.Next;
+            }
+            return temp;
+        }
+
+        //If the next cell is non-null, and conatins the given key, set the out parameter to the associated value, and return true.
+        //Otherwise, Set the out parameter to the default value, and return false.
+
+
+        public bool TryGetValue(TKey k, out TValue v)
+        {
+            CheckNull(k);
+            LinkedListCell<KeyValuePair<TKey, TValue>> cell = GetLastCell(k);
+            if (cell.Next != null && cell.Next.Data.Key.CompareTo(k) == 0)
+            {
+                v = cell.Next.Data.Value;
+                return true;
+            }
+            else
+            {
+                v = default(TValue);
+                return false;
+            }
+        }
+
+        //Essentially the opposite of TRygetValue
+        // If the last key is not null, and contains the given key
+        //   throw an argumentExceptionargument.
+        //      Otherwise, set its data to a new kayvaluepair, and insert a new cell
+        public void Add(TKey k, TValue v)
+        {
+            CheckNull(k);
+            LinkedListCell<KeyValuePair<TKey, TValue>> cell = GetLastCell(k);
+            if (cell.Next != null && cell.Next.Data.Key.CompareTo(k) == 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                LinkedListCell<KeyValuePair<TKey, TValue>> newCell = new LinkedListCell<KeyValuePair<TKey, TValue>>();
+                KeyValuePair<TKey,TValue> newPair = new KeyValuePair<TKey,TValue>(k,v);
+                newCell.Data = newPair;
+                newCell.Next = cell.Next;
+                cell.Next = newCell;
+            }
+        }
+    }
+}
